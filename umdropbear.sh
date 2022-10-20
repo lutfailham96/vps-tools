048482e3a033360095496a7ee134d8ce8ba8eeea
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-10-20 12:08:46 +0700

    Add send notification to telegram

--- umdropbear.sh
+++ umdropbear.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+lock_file="/tmp/umdropbear.lock"
+
+if [[ -f "${lock_file}" ]]; then
+  echo "Unable to lock (${lock_file}), is another process using it?"
+  exit 1
+fi
+
+touch "${lock_file}"
+
 kill_users=""
 max_multi_login=1
 kill_multi_login=false
@@ -26,6 +35,9 @@ dropbear_pids=$(grep 'Password auth succeeded' /var/log/messages | sed "s/'//g;s
 dropbear_active_users=""
 dropbear_pcs=$(pidof dropbear)
 
+telegram_bot_token=""
+telegram_group_id=""
+
 while IFS= read -r dropbear_pid; do
   pid=$(echo "${dropbear_pid}" | awk -F ',' '{print $1}')
   user=$(echo "${dropbear_pid}" | awk -F ',' '{print $2}')
@@ -35,6 +47,23 @@ while IFS= read -r dropbear_pid; do
     dropbear_active_users+="${user}"$'\t\t'"${pid}"$'\t'"${ip}"$'\n'
   fi
 done <<< "${dropbear_pids}"
+
+telegram_send_notification() {
+  bot_token="${1}"
+  group_id="${2}"
+  message_text="${3}"
+  json_body=$(cat <<EOF
+{
+  "chat_id": "${group_id}",
+  "text": "${message_text}"
+}
+EOF
+)
+  curl -sl -X POST \
+    -H 'Content-Type: application/json' \
+    -d "${json_body}" \
+    "https://api.telegram.org/bot${telegram_bot_token}/sendMessage"
+}
 
 dropbear_kill_user() {
   user="${1}"
@@ -65,15 +94,25 @@ if "${kill_multi_login}"; then
   echo "Removing multiple login ..."
   logins=$(echo "${dropbear_active_users}")
   login_users=$(echo "${logins}" | awk '{print $1}' | uniq -c)
-  for login in "${login_users[@]}"; do
+  multiple_detected_count=0
+  multiple_detected_users=""
+  while IFS= read -r login; do
     login_user=$(echo "${login}" | awk '{print $2}')
     login_count=$(echo "${login}" | awk '{print $1}')
-    #login_user=$(echo "${logins}" |  | awk '{print $2}')
     if [[ ${login_count} > ${max_multi_login} ]]; then
       echo "Multiple login detected from user: ${login_user}"
       dropbear_kill_user "${login_user}" "${max_multi_login}"
+      multiple_detected_count=$((${multiple_detected_count}+1))
+      multiple_detected_users+="${multiple_detected_count}. ${login_user}	\[${login_count} login\]"
     fi
-  done
+  done <<< "${login_users}"
+  if [[ ! -z "${telegram_bot_token}" && ! -z "${telegram_group_id}" && ! -z "${multiple_detected_users}" ]]; then
+    echo "Report multiple login user notification to Telegram ..."
+    machine_name=$(hostname)
+    telegram_send_notification "${telegram_bot_token}" "${telegram_group_id}" "Multiple login user \[${machine_name}\]:\n${multiple_detected_users}" 2>&1 > /dev/null &
+  else
+    echo "Cannot send to Telegram, please check Telegram setting"
+  fi
 fi
 
 dropbear_active_users=$(echo "${dropbear_active_users}" | sed '/^[[:space:]]*$/d' | sort)
@@ -87,4 +126,6 @@ ${dropbear_active_users}
 ----------------------------------------
 Active user:	$(echo "${dropbear_active_users}" | wc -l)
 EOF
+
+rm -f "${lock_file}"
 
